fa7d158f81a883160bbeddf2b952b4da6c001848
wangbin <yowangbin@gmail.com>
2016-05-28 23:30:03 +0800

    Signed-off-by: wangbin <yowangbin@gmail.com>

--- Maze.js
+++ Maze.js
@@ -1,9 +1,53 @@
-function Maze() {
+var WIDTH = HEIGHT = 10;
+var pathWidth = 20;
+var book = [];
+var path = [];
+var wall = [];
 
+function dfs(x, y) {
+    path.push([x, y]);
+    book[x * WIDTH + y] = generateRandomDirections();
+    for (var i = 0; i < 4; i++) {
+        nx = x + book[x * WIDTH + y][i][0];
+        ny = y + book[x * WIDTH + y][i][1];
+        if (nx < 0 || nx > 1 * WIDTH - 1 || ny < 0 || ny > 1 * HEIGHT - 1) continue;
+        if (!book[nx * WIDTH + ny]) {
+            wall.push([(x + nx) / 2, (y + ny) / 2]);
+            dfs(nx, ny);
+        }
+    }
 }
-var WIDTH = 4,
-    HEIGHT = 4;
 
-function dsn(x, y) {
-
+function generateRandomDirections() {
+    var steps = [
+            [1, 0],
+            [-1, 0],
+            [0, 1],
+            [0, -1]
+        ],
+        s = [],
+        copyS = [];
+    for (var i = 0; i < 3; i++) {
+        var n = Math.floor(Math.random() * 4);
+        copyS[i] = steps[i];
+        steps[i] = steps[n];
+        steps[n] = copyS[i];
+    }
+    return steps;
 }
+dfs(0, 0);
+console.log(path.toString())
+var canvas = document.getElementById('test');
+canvas.height = 800;
+canvas.width = 800;
+ctx = canvas.getContext('2d');
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, 800, 800);
+ctx.fillStyle = 'white';
+for (var j = 0; j < wall.length; j++) {
+    ctx.fillRect(wall[j][0] * 20, wall[j][1] * 20, 10, 10)
+}
+for (var j = 0; j < path.length; j++) {
+    ctx.fillRect(path[j][0] * 20, path[j][1] * 20, 10, 10)
+}
+ctx.stroke();
--- test.js
+++ test.js
@@ -6,17 +6,17 @@
  var step = 40;
  ctxTest.fillStyle = 'black';
  ctxTest.fillRect(0, 0, 900, 900)
- ctxTest.lineWidth = 30;
+ ctxTest.lineWidth = 4;
  ctxTest.strokeStyle = 'white';
  ctxTest.beginPath();
- ctxTest.moveTo(30, 30);
- ctxTest.lineTo(30, 30 + step);
- ctxTest.lineTo(30 + step, 30 + step);
- ctxTest.lineTo(30 + step * 2, 30 + step);
- ctxTest.lineTo(30 + step * 2, 30 + step * 2);
- ctxTest.lineTo(30 + step, 30 + step * 2);
- ctxTest.lineTo(30, 30 + step * 2);
- ctxTest.lineTo(30, 30 + step * 3);
- ctxTest.lineTo(30 + step, 30 + step * 3);
-
+ ctxTest.moveTo(0, 0);
+ ctxTest.lineTo(6, 0);
+ ctxTest.lineTo(12, 0);
+ ctxTest.lineTo(18, 0);
+ ctxTest.lineTo(18, 6);
+ ctxTest.lineTo(12, 6);
+ ctxTest.lineTo(6, 6);
+ ctxTest.lineTo(0, 6);
+ ctxTest.lineTo(0, 12);
+ // 0,0,6,0,12,0,18,0,18,6,12,6,6,6,0,6,0,12,6,12,12,12,18,12,18,18,12,18,6,18,0,18
  ctxTest.stroke();
